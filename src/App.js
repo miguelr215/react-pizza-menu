450b9e5c058bec1b8fb0a75fb3a15c124785ee59
miguelr215 <drexelramos@gmail.com>
2024-01-31 06:25:28 -0500

    imgs and styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,55 @@
 import './App.css';
+import { ReactDOM } from 'react';
+import './index.css';
+
+const pizzaData = [
+  {
+    name: 'Focaccia',
+    ingredients: 'Bread with italian olive oil and rosemary',
+    price: 6,
+    photoName: 'pizzas/focaccia.jpg',
+    soldOut: false,
+  },
+  {
+    name: 'Pizza Margherita',
+    ingredients: 'Tomato and mozarella',
+    price: 10,
+    photoName: 'pizzas/margherita.jpg',
+    soldOut: false,
+  },
+  {
+    name: 'Pizza Spinaci',
+    ingredients: 'Tomato, mozarella, spinach, and ricotta cheese',
+    price: 12,
+    photoName: 'pizzas/spinaci.jpg',
+    soldOut: false,
+  },
+  {
+    name: 'Pizza Funghi',
+    ingredients: 'Tomato, mozarella, mushrooms, and onion',
+    price: 12,
+    photoName: 'pizzas/funghi.jpg',
+    soldOut: false,
+  },
+  {
+    name: 'Pizza Salamino',
+    ingredients: 'Tomato, mozarella, and pepperoni',
+    price: 15,
+    photoName: 'pizzas/salamino.jpg',
+    soldOut: true,
+  },
+  {
+    name: 'Pizza Prosciutto',
+    ingredients: 'Tomato, mozarella, ham, aragula, and burrata cheese',
+    price: 18,
+    photoName: 'pizzas/prosciutto.jpg',
+    soldOut: false,
+  },
+];
 
 function App() {
   return (
-    <div>
+    <div className="container">
       <Header />
       <Menu />
       <Footer />
@@ -11,31 +58,44 @@ function App() {
 }
 
 function Header() {
-  return <h1>Big Pizza</h1>;
+  return (
+    <header className="header">
+      <h1>Big Pizza</h1>
+    </header>
+  );
 }
 
 function Menu() {
   return (
-    <div>
-      <h2>Menu</h2>
+    <main className="menu">
+      <h2>Our Menu</h2>
       <Pizza />
       <Pizza />
       <Pizza />
-    </div>
+    </main>
   );
 }
 
 function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+
   return (
-    <footer>{new Date().toLocaleTimeString()}, We're currently open!</footer>
+    <footer className="footer">
+      {new Date().toLocaleTimeString()},{' '}
+      {isOpen ? "We're currently open!" : "We're currently closed"}
+    </footer>
   );
 }
 
 function Pizza() {
   return (
     <div>
-      <img src="public/pizzas/spinaci.jpg" alt="pizza" />
-      <h2>Pizza Spinaci</h2>
+      <img src="pizzas/spinaci.jpg" alt="pizza" />
+      <h3>Pizza Spinaci</h3>
+      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
     </div>
   );
 }
